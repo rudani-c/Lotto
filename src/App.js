1f364226c864d013d2cecb4a05f21f9579223be1
rudani-c <rudani.c@outlook.com>
2021-09-25 20:08:36 -0400

    data setup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,74 @@
 import React from 'react';
 import DataTable from 'react-data-table-component';
 import data from './data.json';
-
+drawTime;
 const columns = [
   {
-    name: 'Name',
-    selector: 'name',
+    name: 'Draw Date',
+    selector: 'drawTime',
     sortable: true,
   },
   {
-    name: 'Phone',
-    selector: 'phone',
-    sortable: true,
+    name: 'n1',
+    selector: 'n1',
   },
   {
-    name: 'Email',
-    selector: 'email',
-    sortable: true,
+    name: 'n2',
+    selector: 'n2',
   },
   {
-    name: 'DOB',
-    selector: 'dob',
+    name: 'n3',
+    selector: 'n3',
+  },
+  {
+    name: 'n4',
+    selector: 'n4',
+  },
+  {
+    name: 'n5',
+    selector: 'n5',
+  },
+  {
+    name: 'pb',
+    selector: 'pb',
+  },
+  {
+    name: '0-10',
+    selector: 'zeroToTen',
+  },
+  {
+    name: '11-20',
+    selector: 'elevenToTwenty',
+  },
+  {
+    name: '21-30',
+    selector: 'twentyoneToThirty',
+  },
+  {
+    name: '31-40',
+    selector: 'thirtyoneToFourty',
+  },
+  {
+    name: '41-50',
+    selector: 'fourtyoneToFifty',
+  },
+  {
+    name: '51-60',
+    selector: 'fiftyoneToSixty',
+  },
+  {
+    name: '61-70',
+    selector: 'sixtyoneToSeventy',
   },
 ];
 
 function App() {
   return (
     <div className="App">
-      <h3>DataTable in React - <a href="https://www.cluemediator.com" target="_blank">Clue Mediator</a></h3>
       <DataTable
-        title="Employees"
+        title="Lotto Analysis"
         columns={columns}
-        data={data}
+        data={[]}
         pagination
         highlightOnHover
       />
