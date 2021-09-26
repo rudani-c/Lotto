83a19453e76e2461bdd921d3aa373cd5ede4354f
CHANDRAKANT RUDANI <rudani.c@outlook.com>
2021-09-25 21:23:45 -0400

    analysis

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React from 'react';
 import DataTable from 'react-data-table-component';
 import data from './data.json';
@@ -81,13 +82,46 @@ const columns = [
   },
 ];
 
+Number.prototype.between = function(a, b) {
+  var min = Math.min(a, b),
+    max = Math.max(a, b);
+
+  return this >= min && this <= max;
+};
+
+function noOfNumbersBetween(numbersArray, min, max) {
+  return numbersArray.filter(n => Number(n).between(min, max)).length;
+}
+
 function App() {
+
+  const powerballData = data.map(d => {
+    const drawingResult = d.results[0].primary;
+    const numbers = drawingResult.slice(0, 5);
+    return {
+      drawTime: moment(d.drawTime).format('MM/DD/YY'),
+      [EPowerBallNumbers.N1]: drawingResult[0],
+      [EPowerBallNumbers.N2]: drawingResult[1],
+      [EPowerBallNumbers.N3]: drawingResult[2],
+      [EPowerBallNumbers.N4]: drawingResult[3],
+      [EPowerBallNumbers.N5]: drawingResult[4],
+      [EPowerBallNumbers.PB]: drawingResult[6],
+      [EAnalysis.zeroToTen]: noOfNumbersBetween(numbers, 0, 10),
+      [EAnalysis.elevenToTwenty]: noOfNumbersBetween(numbers, 11, 20),
+      [EAnalysis.twentyoneToThirty]: noOfNumbersBetween(numbers, 21, 30),
+      [EAnalysis.thirtyoneToFourty]: noOfNumbersBetween(numbers, 31, 40),
+      [EAnalysis.fourtyoneToFifty]: noOfNumbersBetween(numbers, 41, 50),
+      [EAnalysis.fiftyoneToSixty]: noOfNumbersBetween(numbers, 51, 60),
+      [EAnalysis.sixtyoneToSeventy]: noOfNumbersBetween(numbers, 61, 70),
+    }
+  });
+
   return (
     <div className="App">
       <DataTable
         title="Lotto Analysis"
         columns={columns}
-        data={[]}
+        data={powerballData}
         pagination
         highlightOnHover
       />
